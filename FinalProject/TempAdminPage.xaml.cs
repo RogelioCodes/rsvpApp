6c745f7523b20cd5753dfd2dd1f47a137667e03f
Vincent Harris <in.vince@hotmail.com>
2020-04-11 13:37:28 -0700

    inital commit

---
+++ FinalProject/TempAdminPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace FinalProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TempAdminPage : ContentPage
+    {
+        public TempAdminPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            DisplayAlert("Welcome!", "This is the admin page.", "Understood");
+        }
+
+        private void tempAdminclick(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://www.youtube.com/watch?v=EQ3clCcwHFc", BrowserLaunchMode.SystemPreferred);
+        }
+    }
+}
